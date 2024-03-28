862a14ba65046dde9ecbba42e2a06f74d7bf4b9b
Paweł Czajczyk <czajczykp@gmail.com>
2024-03-28 21:03:30 +0100

    3/4 something is wrong, maybe like "studio.h"

--- lab4/ids_template.c
+++ lab4/ids_template.c
@@ -50,26 +50,82 @@ int find_idents() {
     int i = 0;
     int id_checked = 0;
     int last_char = 0;
+
+    int find_block_close = 0; // checks if "/*" is currently open
+    int open_comment = 0; // checks if any comm is open
+    int lin_comm = 0; // checks if "//" is open
+
+    int d_quote = 0;
+    int quote = 0;
+    int open_quote = 0;
+
     char string[MAX_ID_LEN];
     while ((digit = getc(stdin)) != EOF) {
-        if ((digit >= 'a' && digit <= 'z') || (digit >= 'A' && digit <= 'Z')
-            || (digit >= '0' && digit <= '9') || digit == '_') {
-            string[i] = (char) digit;
-            i++;
-        } else {
-            if (unique((char *) string, id_checked) == 1 && i > 0) { // if string appears first time
-                sprintf(identifiers[id_checked], "%s", string);
-                id_checked++;
-            }
-            i = 0;
-            memset(string, 0, sizeof(string));
-        }
-        last_char = digit;
-    }
-    for (int j = 0; j < id_checked; ++j) {
-        printf("%s, ", identifiers[j]);
+        int change_last = 1;
+        // quote handle
+        if (open_comment == 0 && digit == '"' && open_quote == 0) {
+            d_quote = 1;
+            open_quote = 1;
 
+        } else if (open_comment == 0 && digit == '"' && d_quote == 1) {
+            d_quote = 0;
+            open_quote = 0;
+            change_last = 0;
+        } else if (open_comment == 0 && digit == '\'' && open_quote == 0) {
+            quote = 1;
+            open_quote = 1;
+        } else if (open_comment == 0 && digit == '\'' && quote == 1) {
+            quote = 0;
+            open_quote = 0;
+            change_last = 0;
+        }
+        if (open_quote == 0) {
+            if (last_char == '/' && digit == '/' && open_comment == 0) { // "//" start
+                open_comment = 1;
+                lin_comm = 1;
+
+                last_char = 0;
+                change_last = 0;
+            } else if (last_char == '/' && digit == '*') { // "/*" start
+                find_block_close += 1;
+                open_comment = 1;
+            } else if (last_char == '*' && digit == '/' && find_block_close >= 1) { // "*/" close
+                if (--find_block_close == 0) {
+                    open_comment = 0;
+                }
+
+                last_char = 0;
+                change_last = 0;
+            }
+            if (digit == '\n' && lin_comm == 1) { // "//" close
+                open_comment = 0;
+                lin_comm = 0;
+            }
+            if (open_comment == 0 && last_char != 92) { // 92 = "\"
+                if (i == 0 && ((digit >= 'a' && digit <= 'z') || (digit >= 'A' && digit <= 'Z') || digit == '_')) { // starts with a letter
+                    string[i] = (char) digit;
+                    i++;
+                } else if (i > 0 && ((digit >= 'a' && digit <= 'z') || (digit >= 'A' && digit <= 'Z') || (digit >= '0' && digit <= '9') || digit == '_')) {
+                    string[i] = (char) digit;
+                    i++;
+                } else {
+                    if (unique((char *) string, id_checked) == 1 && i > 0) { // if string appears first time
+                        sprintf(identifiers[id_checked], "%s", string);
+                        id_checked++;
+                    }
+
+                    i = 0;
+                    memset(string, 0, sizeof(string));
+                }
+            }
+        }
+
+        if (change_last == 1) last_char = digit;
+        else last_char = 0;
     }
+//    for (int j = 0; j < id_checked; ++j) {
+//        printf("%s, ", identifiers[j]);
+//    }
     return id_checked;
 
 }
@@ -90,4 +146,3 @@ int main(void) {
     printf("%d\n", find_idents());
     return 0;
 }
-
